d3203feb82c17c63228d009e6b7b1d79e2f0b42f
ml1908 <ml1908@gmx.at>
2023-09-28 15:07:10 +0200

    schwere Aufgabe :)

---
+++ src/Aufgabe2.java
@@ -0,0 +1,20 @@
+import javax.crypto.spec.PSource;
+import javax.sound.midi.Soundbank;
+
+public class Aufgabe2 {
+    public static void main(String[] args){
+
+        int a = 5;
+        int b = 3;
+
+
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println((double)a*(double)b);
+        System.out.println((double)a/(double)b);
+        System.out.println(a%b);
+
+
+
+    }
+}
